1b1347dd5406b6b3e47184199330b1a81b64c992
Erik Michelson <software@erik.michelson.eu>
2024-06-21 11:28:26 +0200

    feat: generate build hashes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import { hashElement } from 'folder-hash'
+import { writeFile } from 'node:fs/promises'
+import { join } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'build-hash',
+      closeBundle: async () => {
+        const buildHashFile = join('dist', 'build-hash.txt')
+        const hash = await hashElement('dist')
+        writeFile(buildHashFile, hash.hash)
+          .then(() => {
+            console.info('Build finished:', hash.hash)
+          })
+          .catch(error => {
+            console.error('Error writing build hash:', error)
+          })
+      }
+    }
+  ]
 })
